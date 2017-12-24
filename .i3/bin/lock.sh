d46724f650adf5b0b9a68994c6281bc7ba7a3136
Tobias Witek <tobi@tobi-wan-kenobi.at>
2017-12-24 08:55:24 +0100

    [i3] Update lock script to Arch Linux paths
    Plus, delete background image.

--- .i3/bin/lock.sh
+++ .i3/bin/lock.sh
@@ -3,7 +3,7 @@
 # adapted from the solutions found in
 # https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
 
-tmpfile="$HOME/.i3/images/.tmplock.png"
+tmpfile="$HOME/.config/i3/images/.tmplock.png"
 
 scrot -m "$tmpfile"
 convert "$tmpfile" -scale 10% -scale 1000% "$tmpfile"
