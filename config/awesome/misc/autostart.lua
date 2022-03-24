bbfe2124fd27aee647c45fc2e1a08744b41482dd
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-03-24 21:15:11 +0100

    [awesome] fix autostart

--- config/awesome/misc/autostart.lua
+++ config/awesome/misc/autostart.lua
@@ -1,9 +1,17 @@
+local awful = require("awful")
+
 local autostart = {}
 
+local function run_once(cmd)
+	pattern = cmd:match("(%S+)")
+	awful.spawn.with_shell(string.format(
+		"pgrep -u $USER -x %s > /dev/null || (%s)", pattern, cmd
+	))
+end
+
 function autostart.init()
-	-- TODO: spawn once
---awful.util.spawn("xbindkeys")
---awful.util.spawn("picom --experimental-backends")
+	run_once("xbindkeys")
+	run_once("picom --experimental-backends")
 end
 
 return autostart
