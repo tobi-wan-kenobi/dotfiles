848c32710682c4369bbfbb746f676a07869557ac
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-04-02 11:32:57 +0200

    [xmonad] initial config commit

---
+++ config/xmonad/xmonad.hs
@@ -0,0 +1,45 @@
+import XMonad
+
+import XMonad.StackSet
+import XMonad.Util.EZConfig
+
+import XMonad.Actions.Promote
+
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
+import XMonad.Layout.Spacing
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+_layout = tiled ||| Mirror tiled ||| Full ||| three_columns
+	where
+		three_columns = magnifiercz' 1.3 $ ThreeColMid nmain delta ratio
+		tiled = magnifiercz' 1.3 $ Tall nmain delta ratio
+		nmain = 1
+		ratio = 1/2
+		delta = 3/100
+
+_config = def
+	{ modMask = mod4Mask
+	, terminal = "kitty"
+	, layoutHook = smartSpacingWithEdge 5 $ _layout
+	}
+	`additionalKeysP`
+	[ ("M-<Return>", spawn "kitty")
+	, ("M-S-f", spawn "firefox")
+	, ("M-s", windows $ swapMaster . focusDown)
+	, ("M-S-<Return>", promote)
+	]
+
+_xmobar_pp :: PP
+_xmobar_pp = def
+
+main :: IO ()
+main = xmonad
+	. ewmhFullscreen
+	. ewmh
+	. withEasySB (statusBarProp "xmobar ~/.config/xmonad/xmobarrc" (pure _xmobar_pp)) defToggleStrutsKey
+	$ _config
