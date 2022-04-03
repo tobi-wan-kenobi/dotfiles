aaa6e87b99370394a9d92d1b0e68d9218fd28dba
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-04-03 12:45:39 +0200

    [xmonad] add grid layout

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -10,6 +10,7 @@ import XMonad.Actions.Promote
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Magnifier
 import XMonad.Layout.Tabbed
+import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Renamed
@@ -27,7 +28,7 @@ import XMonad.Hooks.StatusBar.PP
 -- Tabbed
 -- Full
 
-three = renamed [Replace "\xfa6c"]
+three = renamed [Replace "\xfc26"]
 	$ noBorders
 	$ smartSpacingWithEdge 5
 	$ magnifiercz' 1.3
@@ -58,6 +59,12 @@ mtiled = renamed [Replace "\xfd35"]
 		ratio = 1/2
 		delta = 3/100
 
+grid = renamed [Replace "\xf5c6"]
+	$ noBorders
+	$ smartSpacingWithEdge 5
+	$ magnifiercz 1.3
+	$ Grid
+
 full = renamed [Replace "\xf792"]
 	$ noBorders
 	$ Full
@@ -65,10 +72,10 @@ full = renamed [Replace "\xf792"]
 tabs = renamed [Replace "\xf9e8"]
 	$ simpleTabbed
 
-_layout = three ||| tiled ||| mtiled ||| full ||| tabs
+_layout = three ||| tiled ||| mtiled ||| grid ||| full ||| tabs
 
 _workspaces :: [String]
-_workspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
+_workspaces = [ "1 \xe795", "2 \xf269", "3 \xe795", "4 \xe795", "5 \xf6ed", "6 \xf9b0", "7 \xf17a", "8 \xf03d", "9 \xfc76", "0 \xfc76" ]
 
 _config = def
 	{ modMask = mod4Mask
@@ -80,7 +87,6 @@ _config = def
 	}
 	`additionalKeysP`
 	[ ("M-<Return>", spawn "kitty")
-	, ("M-S-f", spawn "firefox")
 	, ("M-s", windows $ swapMaster . focusDown)
 	, ("M-S-<Return>", promote)
 	]
